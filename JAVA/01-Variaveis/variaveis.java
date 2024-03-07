43c4067cd236eb0e1391efc5ccb281bea98922ba
Henrique <henriquebrzal@gmail.com>
2024-03-07 14:18:02 -0300

    Declaração de Variáveis em Java

---
+++ JAVA/01-Variaveis/variaveis.java
@@ -0,0 +1,16 @@
+public class Variaveis {
+	
+	public static void main(String[]args) {
+		
+		int idadeUsuario = 17;  
+		String nomeUsuario = "Maria";
+		String nomeBairroUsuario = "Centro";
+		float salarioUsuario = 1750.17F;
+		double saldoDaContaUsuario = 33999.57D;
+		char sexoUsuario = 'f';
+		boolean usuarioCadastrado = true;
+        long populacaoDoPais = 19872397891L; 
+        short quantidadeDeFamiliaresUsuario = 21;
+	}
+
+}
