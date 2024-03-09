58cb36afe6d8598d00e93a38875ea815abc642f0
Henrique <henriquebrzal@gmail.com>
2024-03-09 13:00:40 -0300

    Tipos Primitivos, Valores Booleanos

---
+++ JAVA/02-Tipos-Primitivos/primitivos-03.java
@@ -0,0 +1,12 @@
+public class TiposPrimitivos {
+	
+	public static void main(String[] args) {
+        
+        //Valores Booleanos
+
+        boolean verdadeiro = true;
+        boolean falso = false;
+	
+	}
+		
+}
