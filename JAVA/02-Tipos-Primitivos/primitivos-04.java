328ddd512c9e747b20f8984093c1700cbf1948e1
Henrique <henriquebrzal@gmail.com>
2024-03-09 13:06:18 -0300

    Tipos Primitivos, Valores Char

---
+++ JAVA/02-Tipos-Primitivos/primitivos-04.java
@@ -0,0 +1,12 @@
+public class TiposPrimitivos {
+	
+	public static void main(String[] args) {
+        
+        //Valores Char
+
+	        boolean verdadeiro = true;
+	        boolean falso = false;
+	
+	}
+		
+}
