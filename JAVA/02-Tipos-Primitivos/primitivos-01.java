b10e2ce0eb2e75f713b698fd83a9cf78fb3d59e5
Henrique <henriquebrzal@gmail.com>
2024-03-09 12:40:12 -0300

    Update primitivos-01.java

---
+++ JAVA/02-Tipos-Primitivos/primitivos-01.java
@@ -0,0 +1,14 @@
+public class TiposPrimitivos {
+	
+	public static void main(String[] args) {
+        
+        //Inteiros
+
+		byte temperatura; // -128 até 127 
+		short matricula;  // -32.768 até 32.767 
+		int idade;        // -2.147.483.648 até 2.147.483.647 
+		long saldo;       // -9.223.372.036.854.775.808 até 9.223.372.036.854.775.807 
+		
+	}
+		
+}
