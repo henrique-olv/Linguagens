15d9d739459d4e7452f52c8a69f22eb091607659
Henrique <henriquebrzal@gmail.com>
2024-03-09 12:49:34 -0300

    Tipos Primitivos, Valores de Pontos Flutuantes

---
+++ JAVA/02-Tipos-Primitivos/primitivos-02.java
@@ -0,0 +1,12 @@
+public class TiposPrimitivos {
+	
+	public static void main(String[] args) {
+        
+        //Pontos Flutuantes
+
+        double valorPassagem = 33.10;
+		float valorIngresso = 7.95f;
+		
+	}
+		
+}
